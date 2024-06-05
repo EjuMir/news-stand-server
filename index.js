0845036b03c3807ce7a33af3a5d80f3ea0be25c8
EjuMir <mirejo101@gmail.com>
2024-06-05 13:15:26 +0600

    mongoDB connection successful and methods are responding

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.cckizs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cckizs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,15 +21,25 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+   
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    
+    // Collections
+    const newsCollection = client.db('newsStand').collection('newsCollection')
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    app.get('/allNews', async (req, res) => {
+        const allNews = await newsCollection.find().toArray();
+        res.send(allNews);
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 
